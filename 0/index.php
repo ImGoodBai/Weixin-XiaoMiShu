f37dd119ddb4dee258f26ee91e5a6a3ccca7a811
baeadmin <baeadmin@baidu.com>
2013-09-15 00:07:04 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -3,7 +3,7 @@
 //define your token
 define("TOKEN", "qwerty2345");
 $wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+//$wechatObj->valid();
 $wechatObj->responseMsg();
 
 class wechatCallbackapiTest
