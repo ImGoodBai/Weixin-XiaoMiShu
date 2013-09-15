76399a80207f967493b961d010eb132847aeba1a
baeadmin <baeadmin@baidu.com>
2013-09-15 23:56:13 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -21,7 +21,7 @@ class wechatCallbackapiTest
         }
     }
   
-  public function youdaoDic($word){
+  public function youdaoTran($word){
 
         $keyfrom = "goodbaiA";    //申请APIKEY时所填表的网站名称的内容
         $apikey = "2122702772";  //从有道申请的APIKEY    
