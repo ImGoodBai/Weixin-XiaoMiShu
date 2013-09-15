04b6505cf7c25304f45a2ae67e3f3b25fddc6d15
baeadmin <baeadmin@baidu.com>
2013-09-16 00:31:01 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -32,9 +32,9 @@ class wechatCallbackapiTest
     $result = json_decode($this->getdata4URL($url_youdao));
 
     //   $errorCode = $result['errorCode'];        
-    $trans = $result['translation'];
-    //$trans = "dddddd";
-        return $result;
+    //   $trans = $result['translation'];
+    	$trans = "dddddd";
+        return $trans;
     /*
         if(isset($errorCode)){
             switch ($errorCode){
