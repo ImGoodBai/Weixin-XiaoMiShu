a61fbafdac044b77403a32e45c75bc9ba163236b
baeadmin <baeadmin@baidu.com>
2013-09-15 22:16:55 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -24,8 +24,8 @@ class wechatCallbackapiTest
  	public function baiduTran()
     {
     	echo "My tran function.";
-    	$word = "你好”;
-		$word_code=urlencode($word);
+      //    	$word = "你好”;
+//		$word_code=urlencode($word);
 //$baidu_url = "http://openapi.baidu.com/public/2.0/bmt/translate?client_id=".$appid."&q=".$word_code."&from=".$from."&to=".$to;
 		echo $baidu_url;
         $text=json_decode($this->language_text($baidu_url));
