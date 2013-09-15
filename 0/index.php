e78b889f2dd5a1b26443c04f9ea155eeffee89a2
baeadmin <baeadmin@baidu.com>
2013-09-16 00:03:21 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -27,8 +27,9 @@ class wechatCallbackapiTest
         $keyfrom = "goodbaiA";    //申请APIKEY时所填表的网站名称的内容
         $apikey = "2122702772";  //从有道申请的APIKEY    
         //有道翻译-json格式
-        $url_youdao = 'http://fanyi.youdao.com/fanyiapi.do?keyfrom='.$keyfrom.'&key='.$apikey.'&type=data&doctype=json&version=1.1&q='.$word; 
-    	$result = json_decode($this->getdata4URL($url_youdao));
+    //$url_youdao = 'http://fanyi.youdao.com/fanyiapi.do?keyfrom='.$keyfrom.'&key='.$apikey.'&type=data&doctype=json&version=1.1&q='.$word; 
+    	$url_youdao = 'http://fanyi.youdao.com/openapi.do?keyfrom='.$keyfrom.'&key='.$apikey.'&type=data&doctype=json&version=1.1&q='.$word; 
+    $result = json_decode($this->getdata4URL($url_youdao));
         $errorCode = $result['errorCode'];        
         $trans = '';
         if(isset($errorCode)){
