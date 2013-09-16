c1fa3ae01f9e69245fcac8a4f4bb9e2356b9ac61
baeadmin <baeadmin@baidu.com>
2013-09-16 23:17:34 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -3,7 +3,8 @@
 //define your token
 define("TOKEN", "qwerty2345");
 $wechatObj = new wechatCallbackapiTest();
-//$wechatObj->valid();
+//$validOjb = new validURL();
+//$validObj->valid();
 $wechatObj->responseMsg();
 
 //echo $wecharObj->baiduDic("你好");
