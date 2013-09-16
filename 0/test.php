fe01a919d52d814a5d46938b50db825dbd3d3437
baeadmin <baeadmin@baidu.com>
2013-09-16 23:41:55 +0800

    commit by edit_adapter

--- 0/test.php
+++ 0/test.php
@@ -36,6 +36,7 @@ class baidu{
         //$url_youdao = 'http://fanyi.youdao.com/fanyiapi.do?keyfrom='.$keyfrom.'&key='.$apikey.'&type=data&doctype=json&version=1.1&q='.$word; 
     	$url_youdao = 'http://fanyi.youdao.com/openapi.do?keyfrom=goodbaiA&key=2122702772&type=data&doctype=json&version=1.1&q=wolf'; 
    echo $url_youdao;
+	echo "-----------------";
 	$resultstr = $this->getdata4URL($url_youdao);
 	echo $resultstr;
     $result = json_decode($resultstr);
