4a1bb2b30930a63f6263628173e68f6820152d10
baeadmin <baeadmin@baidu.com>
2013-09-16 22:34:06 +0800

    commit by edit_adapter

--- 0/test.php
+++ 0/test.php
@@ -1,8 +1,48 @@
 <?php
-  
+   public function responseMsg()
+    {
+		if (!empty($postStr)){
+                
+                $fromUsername = $_POST[ "FromUserName" ];
+                $toUsername = $_POST[ "ToUserName" ];
+                $keyword = trim($_POST[ "Content" ];
+                $msgType = trim($_POST[ "MsgType" ];
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";             
+				if(!empty( $keyword ))
+                {
+              		$msgType = "text";
+                  //$contentStr = "Welcome to wechat world!";
+                  //$contentStr = $keyword;
+                    $contentStr = $this->baiduTran($keyword);
+                  //  $contentStr = $this->youdaoTran($keyword);
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
+    }
+
+
+
+
 $name = $_POST[ 'name' ];
 $passwd = $_POST[ 'passwd'  ];
 echo "name: ".$name;
 echo "\npasswd: ".$passwd;
+
+
 
 ?>
