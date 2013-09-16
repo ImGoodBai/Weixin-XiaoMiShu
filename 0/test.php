da2f7d9067fa5759dbeaa10ce73569af808a9553
baeadmin <baeadmin@baidu.com>
2013-09-16 23:56:17 +0800

    commit by edit_adapter

--- 0/test.php
+++ 0/test.php
@@ -33,7 +33,7 @@ class baidu{
         $keyfrom = "goodbaiA";    //申请APIKEY时所填表的网站名称的内容
         $apikey = "2122702772";  //从有道申请的APIKEY    
         //有道翻译-json格式
-        $url_youdao = 'http://fanyi.youdao.com/fanyiapi.do?keyfrom='.$keyfrom.'&key='.$apikey.'&type=data&doctype=json&version=1.1&q='.$word; 
+        $url_youdao = 'http://fanyi.youdao.com/openapi.do?keyfrom='.$keyfrom.'&key='.$apikey.'&type=data&doctype=json&version=1.1&q='.$word; 
     	//$url_youdao = 'http://fanyi.youdao.com/openapi.do?keyfrom=goodbaiA&key=2122702772&type=data&doctype=json&version=1.1&q=wolf'; 
    echo $url_youdao;
 	echo "-----url------------";
