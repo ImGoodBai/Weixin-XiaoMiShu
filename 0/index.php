0881bcbc1cf724a6f2ccf3eba450ee4db6e09310
baeadmin <baeadmin@baidu.com>
2013-09-17 19:45:43 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require( 'translate.php' );
+require( 'response.php' );
 //define your token
 define("TOKEN", "qwerty2345");
 
@@ -10,47 +10,5 @@ define("TOKEN", "qwerty2345");
 $responseObj = new response();
 $responseObj->responseMsg();
 
-class response{
 
-    public function responseMsg()
-    {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-      	//extract post data
-		if (!empty($postStr)){
-                
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                  //$contentStr = "Welcome to wechat world!";
-                  //$contentStr = $keyword;
-                   // $contentStr = $this->baiduTran($keyword);
-                  $translateOBJ = new translate();                  
-                  $contentStr = $translateOBJ->youdaoTran($keyword);
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
-
-        }else {
-        	echo "";
-        	exit;
-        }
-    }
-}
 ?>
