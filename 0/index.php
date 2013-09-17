16db0da6d143fd86337d59f241ecbc2890086cf3
baeadmin <baeadmin@baidu.com>
2013-09-17 19:22:21 +0800

    commit by edit_adapter

--- 0/index.php
+++ 0/index.php
@@ -2,6 +2,7 @@
 
 //define your token
 define("TOKEN", "qwerty2345");
+
 //$validOjb = new validURL();
 //$validObj->valid();
 
@@ -120,36 +121,6 @@ class wechatCallbackapiTest{
         }
     }
 }
-//验证URL
-class validURL{
-  public function valid()
-    {
-        $echoStr = $_GET["echostr"];
 
-        //valid signature , option
-        if($this->checkSignature()){
-        	echo $echoStr;
-        	exit;
-        }
-    }
-	private function checkSignature()
-	{
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
-        		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
-		sort($tmpArr);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
-}
 
 ?>
